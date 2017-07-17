c62fef53513021aba0856e63b00226a67c284bbb
WilfredLemus <wilfred.lean.15@gmail.com>
2017-07-16 20:54:23 -0600

    Live angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,68 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
+import { AppComponent, DialogContent } from './app.component';
+
+import {
+      MdSidenavModule,
+      MdToolbarModule,
+      MdButtonModule,
+      MdIconModule,
+      MdTooltipModule,
+      MdButtonToggleModule,
+      MdMenuModule,
+      MdCardModule,
+      MdInputModule,
+      MdDatepickerModule,
+      MdNativeDateModule,
+      MdCheckboxModule,
+      MdRadioModule,
+      MdSelectModule,
+      MdSlideToggleModule,
+      MdTabsModule,
+      MdChipsModule,
+      MdProgressSpinnerModule,
+      MdProgressBarModule,
+      MdDialogModule,
+      MdSnackBarModule
+      } from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogContent
   ],
+  entryComponents: [ DialogContent ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+
+    // MATERIAL
+    MdSidenavModule,
+    MdToolbarModule,
+    MdButtonModule,
+    MdIconModule,
+    MdTooltipModule,
+    MdButtonToggleModule,
+    MdMenuModule,
+    MdCardModule,
+    MdInputModule,
+    MdDatepickerModule,
+    MdNativeDateModule,
+    MdCheckboxModule,
+    MdRadioModule,
+    MdSelectModule,
+    MdSlideToggleModule,
+    MdTabsModule,
+    MdChipsModule,
+    MdProgressSpinnerModule,
+    MdProgressBarModule,
+    MdDialogModule,
+    MdSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
